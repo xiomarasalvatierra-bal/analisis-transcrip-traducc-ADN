9163be6192eb3815513aa9d15fc6644f2b16727c
Xiomara Pilar Salvatierra Balbin <xiomara.salvatierra@utec.edu.pe>
2026-03-14 23:44:58 -0500

    Add input validation for DNA sequences

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from dna_tookit import analisis_adn, transcripcion_adn, traducir_codones, detectar_mutaciones
 
 #---------------------ANÁLISIS GENERAL DEL ADN-----------------
-adn = input("Ingrese la secuencia ADN: ").upper()
+while True:
+    adn = input("Ingrese la secuencia ADN: ").upper()
 
+    if set(adn).issubset({"A","C","G","T"}):
+        break
+    else:
+        print("Error: Ingresa solo A, C, G o T.")
+
+print("Secuencia aceptada:", adn)
+
+#--------CONTENIDO DE GC---------
 contenido_gc, a, c, t, g, tamanio_adn = analisis_adn(adn)
 
 print("Tamaño del ADN: ", tamanio_adn)
@@ -25,8 +34,24 @@ print("Proteínas: ", proteina)
 
 #-------------------RECONOCER MUTACIONES-----------------------
 
-ref = input("Ingrese la secuencia de referencia: ").upper()
-muestra = input("Ingrese la secuencia a comparar: ").upper()
+ref = adn
+print("Referencia: ", ref)
+
+while True:
+    muestra = input("Ingrese la secuencia a comparar: ").upper()
+
+    if not set(muestra).issubset({"A","C","G","T"}):
+        print("Error: Ingresa solo A, C, G o T.")
+        continue
+
+    if len(muestra) != len(ref):
+        print("Error: Ingresa la misma cantidad de bases nitrogenadas que la referencia.")
+        continue
+
+    break
+
+print("Secuencia a comparar:", muestra)
+
 
 mutaciones = detectar_mutaciones(ref, muestra)
 
